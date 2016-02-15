5f95746b88bf52ed83a8d7ad9debfa5544da9a43
aliserver <aliserver>
2016-02-15 15:31:18 +0800

    add

---
+++ backup/env.php
@@ -0,0 +1,58 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'blackbox',
+  ),
+  'install' => 
+  array (
+    'date' => 'Thu, 14 Jan 2016 07:48:05 +0000',
+  ),
+  'crypt' => 
+  array (
+    'key' => 'd4440e786b890ab02d907f9c6634be96',
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => 'localhost',
+        'dbname' => 'm2ce',
+        'username' => 'root',
+        'password' => 'b229a61b7d',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'default',
+  'cache_types' => 
+  array (
+    'config' => 1,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 1,
+    'reflection' => 1,
+    'db_ddl' => 1,
+    'eav' => 1,
+    'config_integration' => 1,
+    'config_integration_api' => 1,
+    'full_page' => 1,
+    'translate' => 1,
+    'config_webservice' => 1,
+  ),
+);
