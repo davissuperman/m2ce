e28e34d11308e64240fbb359e16c7a07f48cea01
Your Name <aaa@old.com>
2016-03-07 20:19:06 +0800

    add

--- vendor/magento/module-catalog/Observer/Productdesc.php
+++ vendor/magento/module-catalog/Observer/Productdesc.php
@@ -3,7 +3,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\Catalog\Model;
+namespace Magento\Catalog\Observer;
 
 use Magento\Framework\Event\ObserverInterface;
 
